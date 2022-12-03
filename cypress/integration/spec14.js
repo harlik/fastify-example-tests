e700a50d19ccf2c1cd6efe1ee88456a3cffb1d44
Olga Svalova <harlozavr@gmail.com>
2022-12-03 17:23:02 +0200

    Day 2 - my solution

--- cypress/integration/spec14.js
+++ cypress/integration/spec14.js
@@ -1,11 +1,24 @@
 /// <reference types="cypress" />
 
 it('reloads the page until it shows Bananas', () => {
-  // visit the page
-  // if it shows the fruit "Bananas", stop
-  // else
-  //   wait for 1 second for clarity
-  //   reload the page
-  //   check again
-  // Tip: use recursion
+  cy.intercept('GET', '/fruit')
+    .as('fruit');
+    
+  function findBanana() {
+    cy.visit('/');
+    cy.wait('@fruit');
+    cy.get('#fruit')
+      .should('not.contain.text', 'loading')
+      .invoke('text')
+      .as('fruitText')
+      .then(fruitText => {
+        cy.log(fruitText);
+        if (fruitText === 'Bananas') 
+          expect(fruitText).to.be.equal('Bananas');
+        else
+          findBanana();
+      })
+  }
+
+  findBanana();
 })
